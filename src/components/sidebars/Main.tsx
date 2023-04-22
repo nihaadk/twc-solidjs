dfc1563ccf5f209e19de2ad66f7c2547fccd0d52
nihaadk <nihad.keric@emineo.ch>
2023-04-22 09:22:09 +0200

    create routing in the app

--- src/components/sidebars/Main.tsx
+++ src/components/sidebars/Main.tsx
@@ -3,6 +3,9 @@ import { CgProfile, CgMoreO } from "solid-icons/cg";
 import { FiMoreHorizontal } from "solid-icons/fi";
 import { IoNotificationsCircleOutline } from "solid-icons/io";
 import { RiMapCompassDiscoverLine } from "solid-icons/ri";
+import { For } from "solid-js";
+import { links } from "./links";
+import { A } from "@solidjs/router";
 
 const MainSidebar = () => {
   return (
@@ -18,63 +21,28 @@ const MainSidebar = () => {
               </div>
               <div class="my-1 w-full flex-it">
                 <nav class="flex-it items-start">
-                  <a class="flex-it items-start flex-grow w-full" href="#">
-                    <div class="menu-item">
-                      <div class="flex-it">
-                        <AiOutlineHome size={24} />
-                      </div>
-                      <div class="mx-4 text-2xl truncate xl:block hidden">
-                        <span class="truncate">Home</span>
-                      </div>
-                    </div>
-                  </a>
-                  <a class="flex-it items-start flex-grow w-full" href="#">
-                    <div class="menu-item">
-                      <div class="flex-it">
-                        <CgProfile size={24} />
-                      </div>
-                      <div class="mx-4 text-2xl truncate xl:block hidden">
-                        <span class="truncate">Profile</span>
-                      </div>
-                    </div>
-                  </a>
-                  <a class="flex-it items-start flex-grow w-full" href="#">
-                    <div class="menu-item">
-                      <div class="flex-it">
-                        <CgMoreO size={24} />
-                      </div>
-                      <div class="mx-4 text-2xl truncate xl:block hidden">
-                        <span class="truncate">More</span>
-                      </div>
-                    </div>
-                  </a>
-                  <a class="flex-it items-start flex-grow w-full" href="#">
-                    <div class="menu-item">
-                      <div class="flex-it">
-                        <IoNotificationsCircleOutline size={24} />
-                      </div>
-                      <div class="mx-4 text-2xl truncate xl:block hidden">
-                        <span class="truncate">Notification</span>
-                      </div>
-                    </div>
-                  </a>
-                  <a class="flex-it items-start flex-grow w-full" href="#">
-                    <div class="menu-item">
-                      <div class="flex-it">
-                        <RiMapCompassDiscoverLine size={24} />
-                      </div>
-                      <div class="mx-4 text-2xl truncate xl:block hidden">
-                        <span class="truncate">Discover</span>
-                      </div>
-                    </div>
-                  </a>
+                  <For each={links}>
+                    {(link) => (
+                      <A
+                        class="flex-it items-start flex-grow w-full"
+                        href={link.href}
+                      >
+                        <div class="menu-item">
+                          <div class="flex-it">{link.icon()}</div>
+                          <div class="mx-4 text-2xl truncate xl:block hidden">
+                            <span class="truncate">{link.name}</span>
+                          </div>
+                        </div>
+                      </A>
+                    )}
+                  </For>
                 </nav>
               </div>
               {/* GLIDER SEND-MESSAGE BUTTON */}
               <div class="my-1 flex-it w-10/12 cursor-pointer">
                 <div class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition">
                   <div class="flex-it flex-row text-xl font-bold text-white items-start justify-center truncate duration-200">
-                    <div>Glide It</div>
+                    <div>Post It</div>
                   </div>
                 </div>
               </div>
---
+++ src/router/index.tsx
@@ -0,0 +1,20 @@
+import {lazy} from 'solid-js';
+
+
+import { Route, Routes } from "@solidjs/router";
+import HomeScreen from "../screens/Home";
+
+const LoginScreen = lazy(() => import("../screens/Login"));
+const RegisterScreen = lazy(() => import("../screens/Register"));
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" component={HomeScreen} />
+      <Route path="/login" component={LoginScreen} />
+      <Route path="/register" component={RegisterScreen} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
